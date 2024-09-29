d7d876151322ae5b1af93f333a2efcd9e7bd89d4
VishnuNPoojary24 <133808886+VishnuNPoojary24@users.noreply.github.com>
2024-09-29 23:16:06 +0530

    Update Home_page.py

--- Home_page.py
+++ Home_page.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 from streamlit_extras.add_vertical_space import add_vertical_space
 from io import StringIO
